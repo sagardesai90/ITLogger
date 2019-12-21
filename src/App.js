8cb904e3a22be1bb68df39c9f624ed71a9f20e29
sagardesai90 <saagardesai@gmail.com>
2019-12-21 13:30:19 -0800

    components & UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import AddBtn from './components/layout/AddBtn'
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 import './App.css';
 
 const App = () => {
@@ -9,9 +16,17 @@ const App = () => {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment >
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
